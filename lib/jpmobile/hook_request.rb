7109462ef770e797fea3da2ea2b4b56aae57757d
Al Hoang <hoanga@alum.rpi.edu>
2009-04-29 02:26:27 +0900

    Comment fix

--- lib/jpmobile/hook_request.rb
+++ lib/jpmobile/hook_request.rb
@@ -1,11 +1,11 @@
 require 'jpmobile/request_with_mobile'
 
-### Handle Rails 2.3 case
+### Handle Rails 2.2 or lower case
 if defined?(ActionController::AbstractRequest)
   class ActionController::AbstractRequest
     include Jpmobile::RequestWithMobile
   end
-### Handle Rails 2.2 or lower case
+### Handle Rails 2.3 case
 else
   class ActionController::Request
     include Jpmobile::RequestWithMobile
