ccf7ab708a35601dd37728195fabfcc353b1059f
Al Hoang <hoanga@alum.rpi.edu>
2009-05-02 23:46:00 +0900

    Better method for handling Rails 2.3 situation
    * The defined? way causes issues when running the test framework
      because it is detecting the AbstractRequest constant and
      trying to do something with it

--- lib/jpmobile/hook_request.rb
+++ lib/jpmobile/hook_request.rb
@@ -1,12 +1,12 @@
 require 'jpmobile/request_with_mobile'
 
 ### Handle Rails 2.2 or lower case
-if defined?(ActionController::AbstractRequest)
+begin
   class ActionController::AbstractRequest
     include Jpmobile::RequestWithMobile
   end
 ### Handle Rails 2.3 case
-else
+rescue NameError
   class ActionController::Request
     include Jpmobile::RequestWithMobile
   end
